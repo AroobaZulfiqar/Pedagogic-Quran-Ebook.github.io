0a4f3cceb6cd4af54a921fdb6b3d2c1e9d3f4543
AroobaZulfiqar <142680244+AroobaZulfiqar@users.noreply.github.com>
2023-08-19 21:49:33 +0100

    LearningApp

---
+++ qurancorpus-20230817T110821Z-001/qurancorpus/src/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+ 
+const Error = () => {
+    return (
+        <>
+        <h4><center> 404 ....Page not found</center></h4>
+        </>
+    );
+
+}; 
+
+export default Error;
